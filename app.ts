f281503ab3317324d52e321923aeb120c7353f98
Nasif Fuad <nasifuad007@gmail.com>
2025-06-15 20:38:46 +0600

    g

--- app.ts
+++ app.ts
@@ -3,11 +3,13 @@ import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import { router } from "./src/route/router";
 import { errorHandler } from "./src/utility/errorHandler";
+import cors from "cors";
 const app = express();
 
 app.use(helmet());
 app.use(cookieParser());
 app.use(express.json());
+app.use(cors());
 app.use(errorHandler);
 app.use("/api", router);
 export default app;
