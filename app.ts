0403a1c64dd6d382e6ef3d9af058a74020096cda
Nasif Fuad <nasifuad007@gmail.com>
2025-06-15 21:41:47 +0600

    dd

--- app.ts
+++ app.ts
@@ -9,12 +9,7 @@ const app = express();
 app.use(helmet());
 app.use(cookieParser());
 app.use(express.json());
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(errorHandler);
 app.use("/api", router);
 export default app;
