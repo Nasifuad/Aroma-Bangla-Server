6224a97f967c621fd708d32a51197d6c34da3244
Nasif Fuad <nasifuad007@gmail.com>
2025-06-16 21:24:13 +0600

    i

--- app.ts
+++ app.ts
@@ -3,18 +3,18 @@ import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import { router } from "./src/route/router";
 import { errorHandler } from "./src/utility/errorHandler";
-import cors from "cors";
+// import cors from "cors";
 const app = express();
 
 app.use(helmet());
 app.use(cookieParser());
 app.use(express.json());
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     credentials: true,
+//   })
+// );
 app.use(errorHandler);
 app.use("/api", router);
 export default app;
