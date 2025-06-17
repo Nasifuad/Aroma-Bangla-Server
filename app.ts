7c6162dd56347917b98fe2a4c37d9df9c7e0c8dc
Nasif Fuad <nasifuad007@gmail.com>
2025-06-17 09:08:45 +0600

    nigga

--- app.ts
+++ app.ts
@@ -1,17 +1,22 @@
 import express from "express";
+import cors from "cors";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import { router } from "./src/route/router";
 import { errorHandler } from "./src/utility/errorHandler";
-import { hardcoreCors } from "./hardcoreheader";
 const app = express();
-
+app.use(
+  cors({
+    origin: "http://localhost:3000", // Adjust this to your frontend's URL
+    credentials: true, // Allow cookies to be sent
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  })
+);
 app.use(helmet());
 app.use(cookieParser());
 app.use(express.json());
 
 // ← “Hardcore” manual CORS
-app.use(hardcoreCors);
 
 app.use("/api", router);
 app.use(errorHandler);
